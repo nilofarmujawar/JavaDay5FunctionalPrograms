d0b470fd9747c2125f56ff3a9ffb316a9f49744a
nilofarmujawar <99306976+nilofarmujawar@users.noreply.github.com>
2022-02-18 19:41:55 +0530

    functional program is done

--- src/com/bridgelab/Distanceof.java
+++ src/com/bridgelab/Distanceof.java
@@ -16,13 +16,19 @@ class Distanceof{
 
         try {                                             // using try nd catch block
             Scanner s = new Scanner(System.in);
+            
             System.out.println("Enter value for x");     //try is code to be tested for error while it is being executed
+            
             int x = s.nextInt();
+            
             System.out.println("Enter value for y");
+            
             int y = s.nextInt();
+            
             calculate(x, y);
         }
-        catch (Exception e)             // code to b executed,if an error occura in the try block
+        
+        catch (Exception e)                                  // code to b executed,if an error occura in the try block
         {
             System.out.println("Enter integer");
         }
